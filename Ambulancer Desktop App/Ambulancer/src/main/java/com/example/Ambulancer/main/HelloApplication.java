baefcf0decbc7889888159254ab153b258f06983
Ammar-flynn <xtylishammar044@gmail.com>
2026-04-28 01:47:40 +0500

    Add files via upload

---
+++ Ambulancer Desktop App/Ambulancer/src/main/java/com/example/Ambulancer/main/HelloApplication.java
@@ -0,0 +1,20 @@
+package com.example.Ambulancer.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/Ambulancer/Home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1366, 768);
+        stage.setTitle("Ambulancer");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
